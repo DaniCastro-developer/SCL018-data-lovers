77d7838ee61342905bc4583ead8f383c5cea98ec
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-09-26 13:39:31 -0300

    código main y data.js para función ordenar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,17 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const orderAZ = (rickandmorty) => {
+let orderName = rickandmorty.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  } 
+  else {
+    return -1;
+  }
+});
+  console.log(1,orderName);
+  return orderName;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+
+
