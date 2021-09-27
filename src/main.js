8cd375d918d92e4966db1c2d182f567d181f8439
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-09-27 11:07:05 -0300

    wallpaper con video

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import {orderAZ} from './data.js'; //importar funciones de data.js//
 import data from './data/rickandmorty/rickandmorty.js';
 const rickandmorty = data.results;
 
-//console.log(orderAZ);//
+console.log(orderAZ,data);
 
 
+//función ocultar mostrar página
+
+document.getElementById("startButton").addEventListener("click", () => {
+    document.getElementById("page1").hidden = true;
+    document.getElementById("page2").hidden = false;
+  }, false);
+  
+document.getElementById("returnButton").addEventListener("click", () => {
+    document.getElementById("page2").hidden = true;
+    document.getElementById("page1").hidden = false;
+  }, false);
