c4c0c47ff0e02e39146ec50f0a3ba8a92444eee2
Lesly Rojas <leslyrojast@gmail.com>
2021-09-27 11:21:03 -0300

    se creó html page 2

--- src/main.js
+++ src/main.js
@@ -7,12 +7,6 @@ console.log(orderAZ,data);
 
 //función ocultar mostrar página
 
-document.getElementById("startButton").addEventListener("click", () => {
-    document.getElementById("page1").hidden = true;
-    document.getElementById("page2").hidden = false;
-  }, false);
-  
-document.getElementById("returnButton").addEventListener("click", () => {
-    document.getElementById("page2").hidden = true;
-    document.getElementById("page1").hidden = false;
-  }, false);
+
+
+ 
