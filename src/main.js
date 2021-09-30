1984329da24b0d06e26993e5a3f7afd5c4535ac1
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-09-30 11:42:43 -0300

    función hidden y pruebas con el encabezado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import orderAZ from './data.js';
+/*import orderAZ from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 export const rickandmorty = data.results;
 
-console.log(orderAZ, data);
+console.log(orderAZ, data);*/
 
+//---------función mostrar-ocultar páginas------ 
 
+document.getElementById("button-start").addEventListener("click", () => {
+    document.getElementById("page1").hidden = true;
+    document.getElementById("page2").hidden = false;
+  }, false);
+
+document.getElementById("return-button").addEventListener("click", () => {
+    document.getElementById("page2").hidden = true;
+    document.getElementById("page1").hidden = false;
+  }, false);
